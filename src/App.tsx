759209bffcd6441341155b525e8ed18bc74a99df
Oleh Smirnov <oleh.smirnov@icloud.com>
2025-06-07 09:21:05 +0100

    Revert "update"
    This reverts commit 09a5118419870fc7e615a2566d8eafc69acaa2c6.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { CartProvider } from "@/providers/CartProvider";
-import { AuthProvider } from "@/contexts/AuthContext";
 import Layout from "@/components/Layout/Layout";
-import { AdminLayout, ProtectedRoute } from "@/components/Admin";
-
-// Public pages
 import HomePage from "@/pages/HomePage";
 import ProductsPage from "@/pages/ProductsPage";
 import ProductDetailPage from "@/pages/ProductDetailPage";
@@ -17,152 +13,32 @@ import FAQPage from "@/pages/FAQPage";
 import LegalPage from "@/pages/LegalPage";
 import SizeGuidePage from "@/pages/SizeGuidePage";
 import NotFoundPage from "@/pages/NotFoundPage";
-
-// Admin pages
-import { AdminLoginPage } from "@/pages/Admin/LoginPage";
-import { AdminDashboardPage } from "@/pages/Admin/DashboardPage";
-import { AdminProductsPage } from "@/pages/Admin/ProductsPage";
-import { ProductFormPage } from "@/pages/Admin/ProductFormPage";
-import { AdminOrdersPage } from "@/pages/Admin/OrdersPage";
-
 import { ScrollToTop } from "./components/ScrollToTop";
 import { ToastRenderer } from "./components/UI";
 
 function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
+    <CartProvider>
+      <Layout>
         <ScrollToTop />
         <Routes>
-          {/* Public Routes */}
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <HomePage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/products"
-            element={
-              <Layout>
-                <ProductsPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/products/:id"
-            element={
-              <Layout>
-                <ProductDetailPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/cart"
-            element={
-              <Layout>
-                <CartPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/checkout"
-            element={
-              <Layout>
-                <CheckoutPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/success"
-            element={
-              <Layout>
-                <SuccessPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/contact"
-            element={
-              <Layout>
-                <ContactPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <Layout>
-                <AboutPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/faq"
-            element={
-              <Layout>
-                <FAQPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/size-guide"
-            element={
-              <Layout>
-                <SizeGuidePage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/privacy"
-            element={
-              <Layout>
-                <LegalPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/terms"
-            element={
-              <Layout>
-                <LegalPage />
-              </Layout>
-            }
-          />
-
-          {/* Admin Routes */}
-          <Route path="/admin/login" element={<AdminLoginPage />} />
-
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute>
-                <AdminLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<AdminDashboardPage />} />
-            <Route path="products" element={<AdminProductsPage />} />
-            <Route path="products/new" element={<ProductFormPage />} />
-            <Route path="products/:id/edit" element={<ProductFormPage />} />
-            <Route path="orders" element={<AdminOrdersPage />} />
-          </Route>
-
-          {/* 404 */}
-          <Route
-            path="*"
-            element={
-              <Layout>
-                <NotFoundPage />
-              </Layout>
-            }
-          />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/success" element={<SuccessPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/size-guide" element={<SizeGuidePage />} />
+          <Route path="/privacy" element={<LegalPage />} />
+          <Route path="/terms" element={<LegalPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
         <ToastRenderer position="top-right" />
-      </CartProvider>
-    </AuthProvider>
+      </Layout>
+    </CartProvider>
   );
 }
 
--- src/components/Admin/index.ts
+++
@@ -1,2 +0,0 @@
-export { AdminLayout } from "./AdminLayout";
-export { ProtectedRoute } from "./ProtectedRoute";
